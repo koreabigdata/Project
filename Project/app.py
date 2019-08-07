f938f57b3cea4109d0fb78e3ffb0005a9854124c
jungyeongseo <pp1312pp@naver.com>
2019-08-07 19:07:24 +0900

    error fix

--- Project/app.py
+++ Project/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 mt_example = ['아차산','관악산','남산']
 @app.route('/')
 def hello_world():
-    return render_template('index.html',mt_example)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
