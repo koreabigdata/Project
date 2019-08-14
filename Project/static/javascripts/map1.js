88347be8684d42db930a3ba1a82472ee3f07ba15
jungyeongseo <pp1312pp@naver.com>
2019-08-14 15:13:52 +0900

    add css

--- Project/static/javascripts/map1.js
+++ Project/static/javascripts/map1.js
@@ -35,6 +35,28 @@ $.get("static/data/data.json", function(data) {
     clusterer.addMarkers(markers);
 });
 
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+
+
+// 지도에 클릭 이벤트를 등록합니다
+// 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+
+    // 클릭한 위도, 경도 정보를 가져옵니다
+    var latlng = mouseEvent.latLng;
+
+    var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
+    message += '경도는 ' + latlng.getLng() + ' 입니다';
+
+    var resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = message;
+
+});
 
 
 
