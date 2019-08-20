3b033614418c843a5caa6fc89b3afc822b19aeb3
brobaik <hhyungi94@gmail.com>
2019-08-20 18:13:56 +0900

    complete search

--- Project/static/javascripts/map1.js
+++ Project/static/javascripts/map1.js
@@ -1,7 +1,78 @@
-var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표시할 div
-    center : new kakao.maps.LatLng(37.27943075229118, 127.01763998406159), // 지도의 중심좌표
-    level : 14 // 지도의 확대 레벨
-});
+//==============================================================================
+// 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
+function showsido(first) {
+    var valname = first.options[first.selectedIndex].text;
+    ps.keywordSearch(valname +' 산', placesSearchCB);
+}
+function showsigungu(sigun) {
+    var first = document.getElementById('selOne');
+    var valname = first.options[first.selectedIndex].text;
+    var sigun = sigun.options[sigun.selectedIndex].text;
+    ps.keywordSearch(valname + sigun +' 산', placesSearchCB);
+}
+
+
+var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+// 지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption);
+
+// 장소 검색 객체를 생성합니다
+var ps = new kakao.maps.services.Places();
+
+// 키워드로 장소를 검색합니다
+ps.keywordSearch('서울시 강남구 산', placesSearchCB);
+
+
+// 키워드 검색 완료 시 호출되는 콜백함수 입니다
+function placesSearchCB (data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        var bounds = new kakao.maps.LatLngBounds();
+
+
+        for (var i=0; i<data.length; i++) {
+            if(data[i].category_name.indexOf('산') !=-1) {
+                displayMarker(data[i]);
+                bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+            }
+        }
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+
+        console.log(data)
+    }
+}
+
+// 지도에 마커를 표시하는 함수입니다
+function displayMarker(place) {
+
+    // 마커를 생성하고 지도에 표시합니다
+    var marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x)
+    });
+
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(marker, 'click', function() {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+        infowindow.open(map, marker);
+    });
+}
+// var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표시할 div
+//     center : new kakao.maps.LatLng(37.27943075229118, 127.01763998406159), // 지도의 중심좌표
+//     level : 14 // 지도의 확대 레벨
+// });
 
 // // 마커 클러스터러를 생성합니다
 // var clusterer = new kakao.maps.MarkerClusterer({
@@ -50,57 +121,46 @@ var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표�
 
 //--------------------------------------------
 // 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
-var infowindow = new kakao.maps.InfoWindow({zIndex:1});
-
-// var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-//     mapOption = {
-//         center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
-//         level: 3 // 지도의 확대 레벨
-//     };
-
-// 지도를 생성합니다
-// var map = new kakao.maps.Map(mapContainer, mapOption);
-
-// 장소 검색 객체를 생성합니다
-var ps = new kakao.maps.services.Places(map);
-
-// 카테고리로 은행을 검색합니다
-// ps.categorySearch('AT4', placesSearchCB, {useMapBounds:true});
-ps.categorySearch('AT4', placesSearchCB, {
-    // Map 객체를 지정하지 않았으므로 좌표객체를 생성하여 넘겨준다.
-    location: new kakao.maps.LatLng(37.70263763644767, 127.12487249641414),
-    // radius : 20000
-    // "x":"127.12487249641414","y":"37.70263763644767"
-});
-// 키워드 검색 완료 시 호출되는 콜백함수 입니다
-function placesSearchCB (data, status, pagination) {
-    var points = []
-    if (status === kakao.maps.services.Status.OK) {
-        console.log(data)
-        for (var i=0; i<data.length; i++) {
-            if(data[i].category_name.indexOf('산') !=-1){
-                displayMarker(data[i]);
-
-            }
-
-        }
-    }
-}
-
-// 지도에 마커를 표시하는 함수입니다
-function displayMarker(place) {
-    // 마커를 생성하고 지도에 표시합니다
-    var marker = new kakao.maps.Marker({
-        map: map,
-        position: new kakao.maps.LatLng(place.y, place.x)
-    });
-
-    // 마커에 클릭이벤트를 등록합니다
-    kakao.maps.event.addListener(marker, 'click', function() {
-        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
-        infowindow.open(map, marker);
-    });
-}
-
-
+// var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+//
+//
+// // 장소 검색 객체를 생성합니다
+// var ps = new kakao.maps.services.Places(map);
+//
+// // 카테고리로 산을 검색합니다.
+// ps.categorySearch('AT4', placesSearchCB, {
+//     // Map 객체를 지정하지 않았으므로 좌표객체를 생성하여 넘겨준다.
+//     location: new kakao.maps.LatLng(37.496295, 127.061416),
+//     // radius : 20000
+//     // "x":"127.12487249641414","y":"37.70263763644767"
+// });
+// // 키워드 검색 완료 시 호출되는 콜백함수 입니다
+// function placesSearchCB (data, status, pagination) {
+//     console.log(data)
+//     if (status === kakao.maps.services.Status.OK) {
+//         console.log(data)
+//         for (var i=0; i<data.length; i++) {
+//             if(data[i].category_name.indexOf('산') !=-1){
+//                 displayMarker(data[i]);
+//
+//             }
+//
+//         }
+//     }
+// }
+//
+// // 지도에 마커를 표시하는 함수입니다
+// function displayMarker(place) {
+//     // 마커를 생성하고 지도에 표시합니다
+//     var marker = new kakao.maps.Marker({
+//         map: map,
+//         position: new kakao.maps.LatLng(place.y, place.x)
+//     });
+//
+//     // 마커에 클릭이벤트를 등록합니다
+//     kakao.maps.event.addListener(marker, 'click', function() {
+//         // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+//         infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+//         infowindow.open(map, marker);
+//     });
+// }
